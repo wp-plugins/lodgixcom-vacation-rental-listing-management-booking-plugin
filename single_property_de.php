9baccff2fe09cb25f95008cfbd1d6a25894cbbbf
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-08-12 17:08:43 +0000

    Video/Virtual Tour
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/branches/development@422725 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- single_property_de.php
+++ single_property_de.php
@@ -50,6 +50,18 @@ if ($this->options['p_lodgix_contact_url_de'] != "")
 	$mail_icon = '<a title="Contact Us" style="margin-left:5px;" href="' . $mail_url  . '"><img src="' . home_url() . $p_plugin_path  . '/images/mail_50.png"></a>';
 }
 
+$video_icon = '';
+if ($property->video_url != '')
+{
+	$video_icon = '<span class="ceebox"><a style="margin-left:5px;" href="' . $property->video_url  . '"><img title="Display Video" src="' . home_url() . $p_plugin_path  . '/images/video_icon.png"></a></span>';
+}
+
+$virtual_tour_icon = '';
+if ($property->virtual_tour_url != '')
+{
+	$virtual_tour_icon = '<a title="" target="_blank" style="margin-left:5px;" href="' . $property->virtual_tour_url  . '"><img title="Display Virtual Tour" src="' . home_url() . $p_plugin_path  . '/images/virtual_tour.png"></a>';
+}
+
 $single_property .= '';
 $single_property .= '<div id="lodgix_property_badge">';
 $single_property .= '<table width="100%">												
@@ -61,7 +73,7 @@ $single_property .= '<table width="100%">
 										<hr>
 										<table width="100%">												
 													<tr>
-														<td id="lodgix_property_badge_icons_left"><a title="Display Google Map" href="' . $permalink . '#map_canvas"><img src="' . home_url() . $p_plugin_path  . 'images/map_50.png"></a>' . $mail_icon . '</td>
+														<td id="lodgix_property_badge_icons_left"><a title="Display Google Map" href="' . $permalink . '#map_canvas"><img src="' . home_url() . $p_plugin_path  . 'images/map_50.png"></a>' . $video_icon . $virtual_tour_icon . $mail_icon . '</td>
 														<td id="lodgix_property_badge_icons_right"><span class="lodgix_nowrap"><img src="' . home_url() . $p_plugin_path  . '/images/no_pets.png" style="' . $pets . '"><img src="' . home_url() . $p_plugin_path  . 'images/no_smoke.png" style="' . $smoking . '"></span></td>
 													</tr>
 										</table>';

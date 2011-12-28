2670bbfa3feb4d0a08ba942096810091e087d1c3
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-28 15:30:10 +0000

    fix calendar
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/branches/development@481516 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.0.56
+Version: 1.0.57
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.0.57: Addional calendar fix
 v1.0.56: Addional calendar fix
 v1.0.55: Added non flash single calendar
 v1.0.54: Added user german amenities

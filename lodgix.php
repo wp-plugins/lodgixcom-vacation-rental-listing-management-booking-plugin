50a5b99fc6713a8862da0fb884acbb3db4232941
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-13 22:39:20 +0000

    Forms 4
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@504527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.0.62
+Version: 1.0.64
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.0.64: Added property id feature
 v1.0.62: Fixed gravity forms bug
 v1.0.61: Added gravity forms properties
 v1.0.60: Add single page content wrapper

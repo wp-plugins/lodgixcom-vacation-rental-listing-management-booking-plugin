2264aa35a7717bd213d9d2b62075ae5551e73096
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-14 17:26:56 +0000

    Fix White Space
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@504907 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.0.65
+Version: 1.0.66
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.0.66: CSS Adjusted
 v1.0.65: Fixed shortcode issue
 v1.0.64: Added property id feature
 v1.0.62: Fixed gravity forms bug

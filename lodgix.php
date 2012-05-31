5c8145cb54155b89116ba11beca3635e54f40616
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-31 02:12:03 +0000

    Google API 3
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@551182 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.0.82
+Version: 1.0.83
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.0.83: Updated Google Maps API Sensor
 v1.0.82: Updated Google Maps API Version Zoom
 v1.0.81: Updated Google Maps API Version
 v1.0.80: Fixed sidebar position

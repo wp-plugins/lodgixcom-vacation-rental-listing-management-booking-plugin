ae3dae90701c2af6c3f33fb759f8f2fdd10f9f36
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-02-15 14:33:30 +0000

    Merge from responsive
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@668489 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.1.15
+Version: 1.1.16
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.1.16: Fixed regressions on the vacation rental listings page
 v1.1.15: Responsive Design
 v1.1.14: Fix lodgix-custom.css path
 v1.1.11: Fix German Description
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: lodgix
 Tags: vacation rentals, vacation rental, listings, website, cms, availability, calendar, booking, system, software, wordpress plugin, vacation homes,cottages, cabins, condos, holiday rentals
 Requires at least: 3.0.0
 Tested up to: 3.5.1
-Stable tag: 1.1.15
+Stable tag: 1.1.16
 
 Build a sophisticated vacation rental website in minutes using vacation rental software from Lodgix.com. 
 
@@ -92,6 +92,8 @@ vacation rental website in a couple hours.
 
 
 == Changelog ==
+= 1.1.16 = 
+* Fixed regressions on the vacation rental listings page
 = 1.1.15 = 
 *	Responsive Design Changes
 = 1.1.14 =

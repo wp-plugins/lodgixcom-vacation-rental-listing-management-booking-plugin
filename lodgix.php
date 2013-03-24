8ac21a16bf8fd0e03303586020a0a8479061d561
lodgix <lodgix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-24 22:29:59 +0000

    From responsive
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/branches/development@686746 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.1.23
+Version: 1.1.24
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.1.24: Altered featured CSS
 v1.1.23: Altered featured CSS
 v1.1.22: Implemented text expander in property listings
 v1.1.21: Fixed search widget

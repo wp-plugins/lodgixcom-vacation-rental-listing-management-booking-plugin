811e1e665f1b3b8ae9b3e41d8f439ad2ba0949c6
ssynchron <ssynchron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-30 07:23:30 +0000

    Fixed default weekly and daily rates options
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@923593 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.2.6
+Version: 1.2.7
 Author: Lodgix 
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.2.7: Fixed default weekly and daily rates options
 v1.2.6: Fixed german translation. Fixed Rental Search Studio option. Added option to not display weekly and monthly rates
 v1.2.5: Fixed jQuery UI tabs calendar conflict
 v1.2.4: Fixed jQuery UI tabs height bug
@@ -3731,6 +3732,7 @@ if (!class_exists('p_lodgix')) {
         if ($old_db_version < 2.2) {
             $this->options['p_lodgix_display_weekly_rates'] = true;
             $this->options['p_lodgix_display_monthly_rates'] = true;
+            $this->saveAdminOptions();
         }
     }               
       
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: lodgix
 Tags: vacation rentals, vacation rental, listings, website, cms, availability, calendar, booking, system, software, wordpress plugin, vacation homes,cottages, cabins, condos, holiday rentals
 Requires at least: 3.0.0
 Tested up to: 3.9.1
-Stable tag: 1.2.6
+Stable tag: 1.2.7
 
 Build a sophisticated vacation rental website in minutes using vacation rental software from Lodgix.com. 
 
@@ -92,6 +92,8 @@ vacation rental website in a couple hours.
 
 
 == Changelog ==
+= 1.2.7 =
+* Fixed default weekly and daily rates options
 = 1.2.6 =
 * Fixed german translation. Fixed Rental Search Studio option. Added option to not display weekly and monthly rates.
 = 1.2.5 =

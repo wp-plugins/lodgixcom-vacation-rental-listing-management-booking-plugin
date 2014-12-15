ba07144ea416ba3aa95c0ada92a2a63f8d21b624
ssynchron <ssynchron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-15 14:39:05 +0000

    Added plugin icon
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@1044993 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.5.4
+Version: 1.5.5
 Author: Lodgix
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.5.5: Added plugin icon
 v1.5.4: Added new plugin settings design
 v1.5.3: Fixed multi unit calendar
 v1.5.2: Added HTTPS support to availability calendars

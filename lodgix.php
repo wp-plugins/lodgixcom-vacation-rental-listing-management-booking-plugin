fca7e2477191c79fb6967268eed6097d4311ef54
ssynchron <ssynchron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-23 15:04:34 +0000

    Fixed mobile tabbed display
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@1097272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 1.7.0
+Version: 1.7.1
 Author: Lodgix
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v1.7.1: Fixed mobile tabbed display
 v1.7.0: Fixed warning string concatenation
 v1.6.9: Fixed default calendar code
 v1.6.8: Added policies last updated date

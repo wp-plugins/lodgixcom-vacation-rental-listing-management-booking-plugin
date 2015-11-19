3b4f7317c5ada6f466dc351bd61437e55b92b632
alexkvv <alexkvv@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-19 19:59:21 +0000

    v2.0.8: Prepared for Wordpress 4.4
    git-svn-id: https://plugins.svn.wordpress.org/lodgixcom-vacation-rental-listing-management-booking-plugin/trunk@1290157 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lodgix.php
+++ lodgix.php
@@ -4,7 +4,7 @@
 Plugin Name: Lodgix.com Vacation Rental Listing, Management & Booking Plugin
 Plugin URI: http://www.lodgix.com/vacation-rental-wordpress-plugin.html
 Description: Build a sophisticated vacation rental website in seconds using the Lodgix.com vacation rental software. Vacation rental CMS for WordPress.
-Version: 2.0.7
+Version: 2.0.8
 Author: Lodgix
 Author URI: http://www.lodgix.com
 
@@ -12,6 +12,7 @@ Author URI: http://www.lodgix.com
 /*
 
 Changelog:
+v2.0.8: Prepared for Wordpress 4.4
 v2.0.7: Improved automatic review titles.
 v2.0.6: Fixed bug in reviews.
 v2.0.5: Added stars and title to reviews.
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: lodgix
 Tags: vacation rentals, vacation rental, listings, website, cms, availability, calendar, booking, system, software, wordpress plugin, vacation homes,cottages, cabins, condos, holiday rentals
 Requires at least: 3.0.0
 Tested up to: 4.4
-Stable tag: 2.0.7
+Stable tag: 2.0.8
 
 Build a sophisticated vacation rental website in minutes using vacation rental software from Lodgix.com. 
 
@@ -92,6 +92,8 @@ vacation rental website in a couple hours.
 
 
 == Changelog ==
+= 2.0.8 =
+Prepared for Wordpress 4.4
 = 2.0.7 =
 Improved automatic review titles.
 = 2.0.6 =
